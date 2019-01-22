473401bfc88e55f105128352587ad1d50706982f
mayurQED42 <mayur.gajar@qed42.com>
2019-01-22 15:59:03 +0530

    added forms

---
+++ login.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <form action="welcome.php" method="POST">
+                Name: <input type="text" name="name" required><br>
+                password: <input type="password" name="pass" required><br>
+                <input type="submit">
+        </form>
+    </body>
+</html>
---
+++ register.php
@@ -0,0 +1,33 @@
+<html>
+    <style>
+        .page{
+            background-color: bisque;
+            
+        }
+        .info{
+            background-color: aqua;
+            margin:5%;
+            color:black;
+            
+        }
+    </style>
+    <body>
+        <form action="register1.php" method="POST">
+            <div class="page">
+            <center font size="10">Register User</center>
+            <div class="info">
+                    Name: <input type="text" name="name" ><br>
+                    Email id: <input type="text" name="email" ><br>
+                    DOB: <input type="text" name="dob" ><br>
+                    Password: <input type="password" name="password"><br>
+                    <input type="submit" value="register" name="register"><br>
+                    <h1>if you are already registered</h1><br>
+                    <button type="submit" formaction="./login.php">LOGIN</button>
+            </div>
+            </div>
+        </form>
+        <?php
+
+        ?>
+    </body>
+</html>
