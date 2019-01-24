7cfc604e639146a7a51f2f4815bfa2a1a8f97e2e
mayurQED42 <mayur.gajar@qed42.com>
2019-01-24 18:21:33 +0530

    checked mail sent

--- User.php
+++ User.php
@@ -3,32 +3,37 @@ function __autoload($class)
 {
     include_once($class.".php");
 }
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-class User{
+
+class User {
+    /**
+     * Helper method for user login.
+     */
     public function login($name,$passs)
-    {
-        $dbh=new myDb();
+    {   
+        $dbh = new myDb();
         $stmt=$dbh->dbh->prepare("SELECT pass FROM reginfo WHERE cname='$name'"); 
         $stmt->execute();
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        //while($r=$stmt->fetch()){
         $r=$stmt->fetch();
-        if(!strcmp($passs,$r['pass']))
+        $p=md5($passs);
+        if(!strcmp($p,$r['pass']))
         {
-            
             return true;
         }
         else
         {
-            return false;
-           
+            return false;  
         }
     }
+
+    /**
+     *Helper method for user registration 
+     */
     public function reguser($name,$email,$dob,$passs)
     {
+        $p=md5($passs);
         $dbh=new myDb();
-        $que="INSERT INTO reginfo (cname, email, dob, pass) VALUES ('$name', '$email', '$dob', '$passs')";
+        $que="INSERT INTO reginfo (cname, email, dob, pass) VALUES ('$name', '$email', '$dob', '$p')";
         $stmt=$dbh->dbh->prepare($que);
         $val=$stmt->execute();
         return $val;
--- register.php
+++ register.php
@@ -46,6 +46,14 @@ else{
         {
             include "myDb.php";
             include "User.php";
+            //mail veerification:sending mail
+            $to      = $_POST['email']; // Send email to our user
+            $subject = 'Signup | Verification'; // Give the email a subject 
+            $message = "hello
+                        your name=".$_POST['name']." and pass=".$_POST['password']."
+                        ";
+            $headers = 'From:gajarmayur@gmail.com' . "\r\n"; // Set from headers
+            mail($to, $subject, $message, $headers); // Send our email
             $u=new User();
             $ress=$u->reguser($_POST['name'],$_POST['email'],$_POST['dob'],$_POST['password']);
             if($ress)
