52c866018de2ac6aa27b4a9397ec8b5d30a63d5e
mayurQED42 <mayur.gajar@qed42.com>
2019-02-01 12:37:12 +0530

    displayed all user info before filtering it

--- filter.php
+++ filter.php
@@ -14,21 +14,29 @@ ini_set('display_errors', 1);
 <form action="" method="POST">
 <a href="welcome.php">back</a><br><br>
 select filter operator:<br>
+<input type="radio" name="myrole" value="all"> display all<br>
 <input type="radio" name="myrole" value="admin"> admin<br>
 <input type="radio" name="myrole" value="newbie"> newbie<br>
 <input type="radio" name="myrole" value="mentor"> mentor<br>
-<input type="radio" name="myrole" value="all"> display all<br>
 <input type="submit" value="display" name="display"><br>
 </form>
 </body>
 </html>
 
 <?php
+    
     if($_SERVER["REQUEST_METHOD"]=="POST")
     {
+        showdata($_POST['myrole']);
+    }
+    else{
+        showdata("all");
+    }
+    function showdata($mrole)
+    {
         $u=new User();
-        $re=$u->myfilter($_POST['myrole']);
-        echo "<table border=1>";
+        $re=$u->myfilter($mrole);
+        echo "<table border=2>";
         echo "<tr><th>Id</th><th>name</th><th>email</th><th>dob</th><th>pass</th><th>hash</th><th>active</th><th>role</th></tr>";
         for ( $row = 0; $row < count($re); $row++ )
         {
